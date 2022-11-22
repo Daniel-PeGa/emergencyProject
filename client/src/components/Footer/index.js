ae7b0b9e76d4ae6cb4b62f250db2070b25cb29f8
Daniel-PeGa <danielperezgarnic@gmail.com>
2022-11-21 20:42:34 -0600

    progress

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">Romans 5:8</div>
+    </footer>
+  );
+};
+
+export default Footer;
